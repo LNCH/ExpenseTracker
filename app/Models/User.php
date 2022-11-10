61de15fbb8faefad3b7fdc6bcbce3054993cef0e
Tom Lynch <tomlynch@TL-DivideBuyMBPro.local>
2022-11-10 21:56:53 +0000

    Split name into first and last

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -41,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return string
+     */
+    public function getNameAttribute(): string
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
